62c37fae8cdb4f00acdeeef1a35015b6820103cf
yebin <yebin5127@gmail.com>
2021-12-21 16:04:00 +0900

    Delete file

--- app/src/contacts/contacts.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('contacts')
-export class ContactsController {
-    @Get()
-    findAll(): string {
-        return 'This action returns all Contacts';
-    }
-}
--- app/src/contacts/contacts.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ContactsService {
-    getAllContacts() {
-        return 'This is All Contacts';
-    }
-}
