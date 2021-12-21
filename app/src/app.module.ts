f68e36d76bb5a0da094b35722bb9c6feed273b59
yebin <yebin5127@gmail.com>
2021-12-21 22:29:13 +0900

    .

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContactsController } from './contacts/contacts.controller';
-import { ContactsService } from './contacts/contacts.service';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController, ContactsController],
-  providers: [AppService, ContactsService],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '89423000',
+    database: 'contacts',
+    entities: [],
+    synchronize: true, // false로 하는 게 안전
+  }),
+ ],
+ controllers: [AppController],
+ providers: [AppService]
 })
 export class AppModule {}
