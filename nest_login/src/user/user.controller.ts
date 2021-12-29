b5758309704606810883037903b4d3a73a5dab37
yebin <yebin5127@gmail.com>
2021-12-29 09:38:54 +0900

    Create nest_login user controller

---
+++ nest_login/src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { brotliDecompress } from 'zlib';
+import { UserDTO } from './user.dto';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userservice: UserService) {}
+
+    @Post()
+    async signiup(@Body() userData: UserDTO): Promise<UserEntity> {
+        return await this.userservice.signUp(userData);
+    }
+
+    @Post('/login')
+    async login(@Body() userData: UserDTO) {
+        return await this.userservice.login(userData);
+    }
+};
