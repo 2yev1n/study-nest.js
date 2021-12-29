1fb6b6b7c353de8890d039d618e5f80dd57951eb
yebin <yebin5127@gmail.com>
2021-12-29 09:37:29 +0900

    Create nest_login src app

---
+++ nest_login/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(), UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
