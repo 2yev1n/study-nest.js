268e6f30da4e12142f75e64453f0fa6c593aa98f
yebin <yebin5127@gmail.com>
2021-12-29 09:37:40 +0900

    Create nest_login main

---
+++ nest_login/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+const PORT = process.env.PORT||3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log(PORT, "번 포트에서 대기 중");
+}
+bootstrap();
