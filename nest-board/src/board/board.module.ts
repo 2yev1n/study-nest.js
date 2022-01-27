393c9d44ec44912ba560397076e18e7183327bcc
yebin <yebin5127@gmail.com>
2022-01-27 22:40:21 +0900

    UPDATE nest-board board module

--- nest-board/src/board/board.module.ts
+++ nest-board/src/board/board.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardController } from './board.controller';
+import { Board } from './board.entity';
+import { BoardService } from './board.service';
 
-@Module({})
+@Module({
+    imports : [TypeOrmModule.forFeature([Board])],
+    providers : [BoardService],
+    controllers: [BoardController]
+})
 export class BoardModule {}
