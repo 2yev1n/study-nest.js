5e21b1f8eb78cb3ed5c823f067f9195ad3bd69b8
yebin <yebin5127@gmail.com>
2022-01-30 21:32:36 +0900

    UPDATE nest-board board controller

--- nest-board/src/board/board.controller.ts
+++ nest-board/src/board/board.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, response } from 'express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.guard';
 import { User } from 'src/user/user.entity';
 import { BoardService } from './board.service';
@@ -17,11 +17,11 @@ export class BoardController {
         @Body() CreateBoardDto: BoardDto,
         @Req() req: Request,
         ) {
-        const post = await this.boardService.CreatePost(
+            await this.boardService.CreatePost(
             CreateBoardDto,
             req.user as User,
             );
-        return post;
+        return { status: 200, message: "게시물 작성 성공" };
     }
 
     @Get()
@@ -36,15 +36,19 @@ export class BoardController {
         return post;
     }
 
+    @UseGuards(JwtAuthGuard)
     @Patch(":board_id")
     async editPost(@Param("board_id") board_id:number, @Body() editBoardDto: BoardDto) {
-        const post = await this.boardService.EditPost(board_id, editBoardDto);
-        return post;
+        await this.boardService.EditPost(board_id, editBoardDto);
+        const post = await this.boardService.GetPost(board_id);
+
+        return { status: 200, message: "수정 성공", post};
     }
 
-    @Delete("board_id")
+    @UseGuards(JwtAuthGuard)
+    @Delete(":board_id")
     async DeletePost(@Param("board_id") board_id: number) {
-        const post = await this.boardService.DeletePost(board_id);
-        return post;
+        await this.boardService.DeletePost(board_id);
+        return { status : 200, message: "삭제 성공" };
     }
 }
