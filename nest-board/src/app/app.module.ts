79b1926c03fa7af0fd998ac7f5241dab9d925c0c
yebin <yebin5127@gmail.com>
2022-06-05 00:44:33 +0900

    MV nest-board app

--- nest-board/src/app/app.module.ts
+++ nest-board/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { Auth } from './auth';
-import { BoardController } from './board/board.controller';
-import { BoardService } from './board/board.service';
-import { BoardModule } from './board/board.module';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
+// import { Auth } from '../auth';
+import { BoardModule } from '../board/board.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, BoardModule],
   controllers: [AppController],
-  providers: [AppService, Auth,],
+  providers: [AppService],
 })
 
 export class AppModule {}
