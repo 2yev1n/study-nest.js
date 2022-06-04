31cffd236ecdc8e7d7fd0d509612e5db3a497107
yebin <yebin5127@gmail.com>
2022-06-05 00:43:35 +0900

    UPDATE nest-board setting

--- nest-board/src/main.ts
+++ nest-board/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
